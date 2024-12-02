18c0aa5d45a4f94cdd8233eea1f375930f975924
OlegKhai <o.i.dovhyi@student.khai.edu>
2024-12-02 21:35:27 +0200

    повідомлення о виправленні

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Java education");
-        System.out.println("education");
         //comment
         System.out.println("DICT");
     }
