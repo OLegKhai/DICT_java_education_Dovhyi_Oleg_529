f890314a31bfdf2ccb7eed04dbf86667ccf905cd
OlegKhai <o.i.dovhyi@student.khai.edu>
2024-12-06 17:53:52 +0200

    MatrixProcessing 2st stage

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -6,25 +6,75 @@ public class MatrixProcessing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Считываем первую матрицу
+        while (true) {
+            // Меню выбора операции
+            System.out.println("\nВыберите операцию:");
+            System.out.println("1. Сложение матриц");
+            System.out.println("2. Умножение матрицы на константу");
+            System.out.println("0. Выход");
+            System.out.print("Ваш выбор: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 0) {
+                System.out.println("Завершение работы программы.");
+                break;
+            }
+
+            switch (choice) {
+                case 1:
+                    addMatrices(scanner);
+                    break;
+                case 2:
+                    multiplyMatrixByConstant(scanner);
+                    break;
+                default:
+                    System.out.println("Неверный выбор. Попробуйте снова.");
+            }
+        }
+    }
+
+    private static void addMatrices(Scanner scanner) {
         System.out.println("Введите размеры первой матрицы:");
         int n1 = scanner.nextInt();
         int m1 = scanner.nextInt();
         int[][] matrixA = readMatrix(scanner, n1, m1);
 
-        // Считываем вторую матрицу
         System.out.println("Введите размеры второй матрицы:");
         int n2 = scanner.nextInt();
         int m2 = scanner.nextInt();
         int[][] matrixB = readMatrix(scanner, n2, m2);
 
-        // Проверка возможности сложения
         if (n1 != n2 || m1 != m2) {
             System.out.println("ERROR");
         } else {
             int[][] result = addMatrices(matrixA, matrixB);
             printMatrix(result);
         }
+    }
+
+    private static void multiplyMatrixByConstant(Scanner scanner) {
+        System.out.println("Введите размеры матрицы:");
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] matrix = readMatrix(scanner, n, m);
+
+        System.out.println("Введите константу:");
+        int constant = scanner.nextInt();
+
+        int[][] result = multiplyMatrix(matrix, constant);
+        printMatrix(result);
+    }
+
+    private static int[][] multiplyMatrix(int[][] matrix, int constant) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] result = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[i][j] = matrix[i][j] * constant;
+            }
+        }
+        return result;
     }
 
     private static int[][] readMatrix(Scanner scanner, int n, int m) {
